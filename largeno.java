3548133a50b54528cb05e6281fea8a589a366ca5
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-07-27 12:04:19 +0530

    Create largeno.java

---
+++ largeno.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x , y , z;
+		Scanner s=new Scanner(System.in);
+		x=s.nextInt();
+		y=s.nextInt();
+		z=s.nextInt();
+		if(x>y && x>z)
+		{
+			System.out.println(x);
+		}
+		else if(y>x && y>z)
+		{
+			System.out.println(y);
+		}
+		else if(z>x && z>y)
+		{
+			System.out.println(z);
+		}
+		
+	}
+}
