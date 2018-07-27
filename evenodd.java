173d4f821e81a71d3f481778e1dc21cc246cbff8
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-07-27 10:57:12 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		char c=s.next().charAt(0);
+		if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+		{
+			System.out.println("vowel");
+		}
+		else if(c>'a' && c<'z')
+		{
+			System.out.println("consonant");
+		}
+		else
+		{
+			System.out.println("invalid");
+		}
+	}
+}
