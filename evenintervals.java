0d0a35f47df23ff15da717ff879d5287ca2eee3c
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-07-31 16:20:09 +0530

    Create evenintervals.java

---
+++ evenintervals.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int Q=s.nextInt();
+		for(int i=N+1;i<Q;i++)
+		{
+			if(i%2==0)
+			System.out.println(i+" ");
+		}
+	}
+}
