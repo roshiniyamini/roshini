f4a286260a493d41e6e3b6f16d79e171697f1578
roshiniyamini <41729316+roshiniyamini@users.noreply.github.com>
2018-08-09 12:07:40 +0530

    Create sum2num.java

---
+++ sum2num.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n1=s.nextInt();
+		int n2=s.nextInt();
+		int sum=0;
+		sum=n1+n2;
+		{
+			System.out.println(sum);
+		}
+	}
+}
